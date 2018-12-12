52a822994c0c4aaba72e121c8fceb7cc086f078b
Vanessa Frenkel <vanefrenkel@gmail.com>
2018-12-12 19:30:44 -0300

    segunda funcion

--- prueba.rb
+++ prueba.rb
@@ -3,11 +3,10 @@ data_students = file.readlines.map(&:chomp)
 file.close
 
 # print products
-
 students_array = data_students.map do |x|
  x.split(',').to_a
 end
-#print students_array
+
 
 # Esta método solo hace un hash, 
 # Su uso es dentro de la función que crea el array
@@ -29,25 +28,31 @@ end
 all_data_student_hash = create_epic_array_of_hashes(students_array)
 
 
- 
-all_data_student_hash.each_index do |x|
-    all_data_student_hash[x][:grades].map {|w| w == ' A' ? 1 : w }
-   print all_data_student_hash[x][:grades].map {|z| z.to_i}
+def sacar_promedio(array_of_hashes)
+    array_of_hashes.each_index do |x|
+        name = array_of_hashes[x][:name]
+        array_of_hashes[x][:grades].map {|w| w == ' A' ? 1 : w }
+        int_grades =  array_of_hashes[x][:grades].map {|z| z.to_f}
+        grades_qua = array_of_hashes[x][:grades].length
+        promedio = int_grades.inject(0) {|sum,n| sum + n /grades_qua }
+        content = "#{name}: #{promedio}"
+        file = File.open("promedios.csv", 'a')
+        file.puts content
+        file.close
+    end
 end
 
+def ausentes(array_of_hashes)
+    array_of_hashes.each_index do |x|
+        name = array_of_hashes[x][:name]
+        a = array_of_hashes[x][:grades].count(" A")
+        puts "#{name} tiene: #{a} ausencias."
+    end   
+end
+ausentes(all_data_student_hash)
 
 
-# sacar el promedio
-# def average_grade(grades_array)
-#     grades_array = grades_array.each_index do |x|
-#         grades_array[x][:grades].map {|w| w == ' A' ? 1 : w }
-#     end
-# end
 
-
-
-
-#average_grade(all_data_student_hash)
 
 
 
@@ -66,9 +71,11 @@ end
 
 # option = 0
 # while option != 4
-#     option = gets.chomp.to_i
+#   program_init()
+#    option = gets.chomp.to_i
 #     case option
 #         when 1
+#           sacar_promedio(all_data_student_hash)
 #         when 2
 #         when 3
 #         else
